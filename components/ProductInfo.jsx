42f76936c3d2314218b60ca72dc0083526fe7ed3
zelkovaria <hyein396@gmail.com>
2025-06-23 22:41:47 +0900

    feat: router 장바구니 페이지 이동

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import styles from "./ProductInfo.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 const ProductInfo = ({ productDetail }) => {
+  const router = useRouter();
+
+  const addCart = () => {
+    router.push("/cart");
+  };
+
   return (
     <div className={styles.container}>
       <div>
-        <Image src={productDetail[0].imageUrl} alt="상품 이미지" />
+        <Image
+          height={250}
+          width={250}
+          src={productDetail[0].imageUrl}
+          alt="상품 이미지"
+        />
       </div>
       <div className={styles.description}>
         <p>{productDetail[0].name}</p>
         <p>{productDetail[0].price}</p>
-        <button>장바구니 담기</button>
+        <button onClick={addCart}>장바구니 담기</button>
       </div>
     </div>
   );
